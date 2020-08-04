29e8dbfa44f16fe1a1b6051df3254648e6654aa7
basavarajsh98 <hiremathsbasavaraj@gmail.com>
2020-08-04 13:53:49 +0530

    Verify the checkout page

---
+++ page_objects/Confirmation_Page.py
@@ -0,0 +1,23 @@
+"""
+This module models the Confirmation page
+URL: /sunscreen
+"""
+from .Base_Page import Base_Page
+from utils.Wrapit import Wrapit
+import conf.locators_conf as locators 
+
+class Confirmation_Page(Base_Page):
+
+    PAYMENT_SUCCESS=locators.PAYMENT_SUCCESS
+
+    "This class models the sunscreen page"
+    def start(self):
+        "Go to this URL -- if needed"
+        url = 'confirmation'
+        self.open(url)
+    
+    def verify_payment(self):
+        result_flag= False
+        if self.PAYMENT_SUCCESS:
+            result_flag= True
+        return result_flag
